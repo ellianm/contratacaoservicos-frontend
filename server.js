c4227283f0a383883c3df88e77b60cc1dc0557a5
ellianm <ellianmarcondes@hotmail.com>
2020-11-09 17:36:25 -0300

    commit 09/11/2020

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/frontend/dist/ContratacaoServicosFrontend' ));
+app.use(express.static(__dirname + '/dist/ContratacaoServicosFrontend' ));
 
 
 app.get('/*', (req,res) => {
-    res.sendfile(__dirname + '/frontend/dist/ContratacaoServicosFrontend/index.html')
+    res.sendfile(__dirname + '/dist/ContratacaoServicosFrontend/index.html')
 })
 
 app.listen(port, () => {
